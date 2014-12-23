fb453a264e9e3da33f1fe49543a4b54c0ff30808
Calvin Lui <clui951@berkeley.edu>
2014-12-22 16:14:08 -0800

    StockObj methods updated; need to implement stock ID look up in API

--- src/StockObj.java
+++ src/StockObj.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+import java.util.Date;
+
 public class StockObj {
 	protected String name;
 	protected String symbol;
-	protected long time;
+	protected String time;
 	protected double price;
 	protected double openPrice;
 	protected double dayHigh;
@@ -30,7 +33,7 @@ public class StockObj {
 			this.openPrice = Double.parseDouble(csvData[3]);
 			this.dayHigh = Double.parseDouble(csvData[4]);
 			this.dayLow = Double.parseDouble(csvData[5]);
-			this.time = 0;
+			this.time = (new Date()).toString();
 			this.valid = true;
 		} catch (Exception e1) {
 //			e1.printStackTrace();
@@ -42,14 +45,20 @@ public class StockObj {
 		if ( !this.valid ) {
 			System.out.println("--- DATA IS CURRENTLY INVALID ---");
 		}
+		
+		// set up name underline
+		int headLen = (this.name + " - " + this.symbol).length();
+		char[] underline = new char[headLen];
+		char underlineSymbol = ("-").charAt(0);				
+		Arrays.fill(underline, underlineSymbol);
+		
 		System.out.println("\n" + this.name + " - " + this.symbol);
+		System.out.println(underline);
 		System.out.println(this.time);
-		System.out.println("Current: $" + this.price);
-		System.out.println("Open: $" + this.openPrice);
+		System.out.printf("%-10s %s", "Current:", "$ " + this.price + "\n");
+		System.out.printf("%-10s %s", "Open:", "$ " + this.openPrice + "\n");
+		System.out.printf("%-10s %s", "High/Low:", "$ " + this.dayHigh +"/" + this.dayLow + "\n\n");
 	}
-
-
-
 
 	public String getSymbol() {
 		return this.symbol;
@@ -59,7 +68,7 @@ public class StockObj {
 		return this.name;
 	}
 
-	public long getTime() {
+	public String getTime() {
 		return this.time;
 	}
 
@@ -67,6 +76,18 @@ public class StockObj {
 		return this.price;
 	}
 
+	public double getOpenPrice() {
+		return this.openPrice;
+	}
+
+	public double getDayHigh() {
+		return this.dayHigh;
+	}
+
+	public double getDayLow() {
+		return this.dayLow;
+	}
+	
 	public String[] getCsvData() {
 		return this.csvData;
 	}
